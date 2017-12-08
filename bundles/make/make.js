14d21f902c3571966db39fa8da2cc21c9e5096ec
Adam Liechty <adam@doubledutch.me>
2017-12-07 16:52:21 -0800

    fix var change

--- bundles/make/make.js
+++ bundles/make/make.js
@@ -6,7 +6,7 @@ const rimraf = require('rimraf')
 const packager = require('../../packager.js')
 const { baseBundleVersion } = require('../../config')
 
-const bundleDir = `../${version}`
+const bundleDir = `../${baseBundleVersion}`
 
 console.log(`\n\n**************************************\n  Creating base bundles in ${bundleDir}\n**************************************\n\n`)
 
@@ -18,19 +18,19 @@ const platforms = ['ios', 'android']
 Promise.all(platforms.map(async platform => {
   const { metroBundle } = await packager({
     entry: './base.js',
-    manifestOut: `${bundleDir}/base.${platform}.${version}.manifest`,
-    out: `${bundleDir}/base.${platform}.${version}.bundle`,
+    manifestOut: `${bundleDir}/base.${platform}.${baseBundleVersion}.manifest`,
+    out: `${bundleDir}/base.${platform}.${baseBundleVersion}.bundle`,
     platform,
     root: process.cwd()
   })
 
-  const bundle = fs.readFileSync(`${bundleDir}/base.${platform}.${version}.bundle.js`, {encoding: 'utf8'})
-  fs.writeFileSync(`${bundleDir}/base.${platform}.${version}.bundle.js`, bundle.replace(/\nrequire\(.+\);?/g, `\n// DoubleDutch base bundle ${platform}.${version}\n`))
+  const bundle = fs.readFileSync(`${bundleDir}/base.${platform}.${baseBundleVersion}.bundle.js`, {encoding: 'utf8'})
+  fs.writeFileSync(`${bundleDir}/base.${platform}.${baseBundleVersion}.bundle.js`, bundle.replace(/\nrequire\(.+\);?/g, `\n// DoubleDutch base bundle ${platform}.${baseBundleVersion}\n`))
 
-  fs.writeFileSync(`${bundleDir}/base.${platform}.${version}.sourcemap`, metroBundle.map)
+  fs.writeFileSync(`${bundleDir}/base.${platform}.${baseBundleVersion}.sourcemap`, metroBundle.map)
 
-  fs.renameSync(`${bundleDir}/base.${platform}.${version}.bundle.js`, `${bundleDir}/base.${platform}.${version}.bundle`)
-  fs.renameSync(`${bundleDir}/base.${platform}.${version}.bundle.js.meta`, `${bundleDir}/base.${platform}.${version}.bundle.meta`)
+  fs.renameSync(`${bundleDir}/base.${platform}.${baseBundleVersion}.bundle.js`, `${bundleDir}/base.${platform}.${baseBundleVersion}.bundle`)
+  fs.renameSync(`${bundleDir}/base.${platform}.${baseBundleVersion}.bundle.js.meta`, `${bundleDir}/base.${platform}.${baseBundleVersion}.bundle.meta`)
 })).then(() => {
   console.log(`\n\n**************************************\n  Done\n**************************************\n\n`)
 })
